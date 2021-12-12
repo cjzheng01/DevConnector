1b9af0ae2f09fb8bb1c817d7760af8b0d9829209
Jiabao Zheng <cjzheng01@gmail.com>
2021-12-12 01:07:27 -0500

    user api routes

--- config/db.js
+++ config/db.js
@@ -5,11 +5,12 @@ const db = config.get('mongoURI');
 const connectDB = async () => {
     try {
         await mongoose.connect(db, {
-            useNewUrlParser : true
+            useNewUrlParser : true,
+            // useCreateIndex: true
         });
         
         console.log('MongoDB Connected...');
-    } catch (err) {
+    } catch (err) { 
         console.error(err.message);
         // Exit process with failure
         process.exit(1);
